6c69c8790b5c1f3bb6e51d41416e256a8043e06f
Victoria Cheng <victoria.cheng15@gmail.com>
2022-01-03 11:18:36 -0500

    add responsive view for desktop

--- src/routes/Destination.jsx
+++ src/routes/Destination.jsx
@@ -29,19 +29,22 @@ const Destination = () => {
 
   return (
     <main className="w-screen h-screen pt-36 md:pt-52 bpCustom:pt-96 text-white bg-no-repeat bg-cover bg-destination-mobile md:bg-destination-tablet bpCustom:bg-destination-desktop">
-      <div className="w-11/12 m-auto max-w-screen-bpCustom">
-        <h1 className="text-xl tracking-widest text-center text-white uppercase font-barlow mb-9">
+      <div className="m-auto max-w-screen-bpCustom">
+        <h1 className="text-3xl tracking-widest text-center text-white uppercase font-barlow mb-9 bpCustom:text-left bpCustom:pl-40 bpCustom:mb-20">
           <span className="mr-3 opacity-60">01</span>
           pick your destination
         </h1>
-        <div className="mb-6 w-[10.625rem] md:w-[18.75rem] aspect-square grid place-items-center m-auto">
-          <img
-            src={`./assets/destination/image-${planetName}.png`}
-            alt={planetName}
-          />
-        </div>
-        <div className="flex items-center justify-center w-full gap-4 mb-7">
-          {
+        <div className="flex flex-col items-center justify-center gap-10 bpCustom:gap-20 bpCustom:flex-row">
+          <div className="mb-6 grid place-items-center">
+            <img
+              className="w-[10.625rem] md:w-[18.75rem] bpCustom:w-[27.8125rem] aspect-square"
+              src={`./assets/destination/image-${planetName}.png`}
+              alt={planetName}
+            />
+          </div>
+          <section className="flex flex-col items-center p-4">
+            <div className="flex items-center justify-center w-full gap-4 mb-7">
+              {
             planetList.map((planet) => (
               <button
                 key={planet.id}
@@ -54,30 +57,30 @@ const Destination = () => {
               </button>
             ))
           }
-        </div>
-        <section className="w-80 md:w-[35.625rem] m-auto grid place-items-center p-4">
-          <h1 className="text-5xl uppercase mb-5 font-bellefair">{planetName}</h1>
-          <p className=" text-base leading-7 text-center mb-8 font-barlow">
-            {
+            </div>
+            <h1 className="text-5xl uppercase mb-5 font-bellefair">{planetName}</h1>
+            <p className=" text-base leading-7 text-center mb-8 font-barlow w-80 md:w-[35.625rem] bpCustom:w-[27.8125rem]">
+              {
               data.map((planet) => (planet.name.toLowerCase() === planetName ? planet.info.description : ''))
             }
-          </p>
-          <div className="w-full h-0.5 bg-white/50 mb-8" />
-          <div className="flex flex-col gap-10 justify-center items-center md:flex-row w-full">
-            <div className="flex flex-col items-center justify-center">
-              <h2 className="text-base uppercase tracking-widest font-barlow mb-4">avg. distance</h2>
-              <p className="text-3xl uppercase">
-                {data.map((planet) => (planet.name.toLowerCase() === planetName ? planet.info.distance : ''))}
-              </p>
+            </p>
+            <div className="w-full h-0.5 bg-white/50 mb-8" />
+            <div className="flex flex-col gap-10 justify-center items-center md:flex-row w-full">
+              <div className="flex flex-col items-center justify-center">
+                <h2 className="text-base uppercase tracking-widest font-barlow mb-4">avg. distance</h2>
+                <p className="text-3xl uppercase">
+                  {data.map((planet) => (planet.name.toLowerCase() === planetName ? planet.info.distance : ''))}
+                </p>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="text-md uppercase tracking-widest font-barlow mb-4">est. travel time</h3>
+                <p className="text-3xl uppercase">
+                  {data.map((planet) => (planet.name.toLowerCase() === planetName ? planet.info.travel : ''))}
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col items-center justify-center">
-              <h3 className="text-md uppercase tracking-widest font-barlow mb-4">est. travel time</h3>
-              <p className="text-3xl uppercase">
-                {data.map((planet) => (planet.name.toLowerCase() === planetName ? planet.info.travel : ''))}
-              </p>
-            </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     </main>
   );
