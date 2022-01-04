b71c935d53ce080cc3186b1b3d86354b7e1db7f0
Victoria Cheng <victoria.cheng15@gmail.com>
2022-01-04 18:05:49 -0500

    create list js

---
+++ src/listsHelper.js
@@ -0,0 +1,46 @@
+export const linkList = [
+  {
+    id: 1,
+    span: '00',
+    href: '/',
+    name: 'home',
+  },
+  {
+    id: 2,
+    span: '01',
+    href: 'destination',
+    name: 'destination',
+  },
+  {
+    id: 3,
+    span: '02',
+    href: 'crew',
+    name: 'crew',
+  },
+  {
+    id: 4,
+    span: '03',
+    href: 'technology',
+    name: 'technology',
+  },
+];
+
+export const planetList = [
+  { id: 1, name: 'moon' },
+  { id: 2, name: 'mars' },
+  { id: 3, name: 'europa' },
+  { id: 4, name: 'titan' },
+];
+
+export const crewList = [
+  { id: 1, name: 'douglas hurley' },
+  { id: 2, name: 'mark shuttleworth' },
+  { id: 3, name: 'victor glover' },
+  { id: 4, name: 'anousheh ansari' },
+];
+
+export const techList = [
+  { id: 1, name: 'launch vehicle' },
+  { id: 2, name: 'spaceport' },
+  { id: 3, name: 'space capsule' },
+];
