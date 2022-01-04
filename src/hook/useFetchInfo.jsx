064340228e88aaf9fe2d6b37c0a689fa7ec261e1
Victoria Cheng <victoria.cheng15@gmail.com>
2022-01-04 18:16:09 -0500

    move data json file to public folder

--- src/hook/useFetchInfo.jsx
+++ src/hook/useFetchInfo.jsx
@@ -5,7 +5,7 @@ export const useFetchInfo = (page) => {
   const [fetchError, setFetchError] = useState(null);
 
   useEffect(() => {
-    const url = './src/data/data.json';
+    const url = './data/data.json';
     const fetchData = async () => {
       try {
         const info = await (await fetch(url)).json();
