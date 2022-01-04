804b14fa75d37235ec4268f53906aa76379f43c9
Victoria Cheng <victoria.cheng15@gmail.com>
2022-01-04 18:06:31 -0500

    create exlpore jsx

---
+++ src/components/travel-info/Explore.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Explore = () => (
+  <div className="relative grid w-40 duration-300 bg-white rounded-full bpCustom:w-72 md:w-60 aspect-square place-items-center content-none after:absolute after:w-full after:h-full after:bg-white/25 after:rounded-full hover:after:scale-150 focus:after:scale-150 ease">
+    <a
+      href="/"
+      className="relative z-30 text-xl tracking-widest uppercase text-clrDark-925"
+    >
+      explore
+    </a>
+  </div>
+);
+
+export default Explore;
