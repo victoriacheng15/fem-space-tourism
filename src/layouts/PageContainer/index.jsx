a9c88c1d1af0cc7ac4ec946605d566fcd843916c
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-10-03 17:54:14 -0600

    Development (#5)
    * docs: add prettier format packages and tailwindcss
    
    * docs: update yarn with new packages
    
    * chore: format files with linter and prettier
    
    * refactor: update file to index.jsx
    
    * refactor: update file name to index.jsx for navigation folder
    
    * refactor: add index.js for import Titles componments
    
    * refactor: update file name to index.js for TravelInfo folder
    
    * refactor: add index.js for import routes componments
    
    * refactor: update componment file paths
    
    * Refactor/break down components (#4)
    
    * refactor: add layouts for main tag, home and page
    
    * refactor: add layouts for home, crew, tech and destination pages and remove comments
    
    * style: format files with formatter

---
+++ src/layouts/PageContainer/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageContainer = ({ children }) => {
+  return <div className="m-auto max-w-screen-bpCustom">{children}</div>;
+};
+
+export default PageContainer;
