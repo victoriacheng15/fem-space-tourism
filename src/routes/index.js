aea3714bd5c572aed9ea7a272c448f9fe8d0ce22
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-03 16:12:05 -0600

    refactor: add index.js for import routes componments

---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+export { default as Crew } from './Crew';
+export { default as Destination } from './Destination';
+export { default as Home } from './Home';
+export { default as Technology } from './Technology';
