ebf88f2786589ee8b345c4c8b95dfa0a1d4a3922
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-03 22:06:31 -0600

    refactor: move classes to imageBox and then pass typeName prop

--- src/components/ImageBox/index.jsx
+++ src/components/ImageBox/index.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
-const ImageBox = ({ classes, src, alt, cover, underline }) => (
-  <div className={classes}>
-    <img
-      className={`${cover ? 'object-cover' : 'object-contain'} h-full w-full`}
-      src={src}
-      alt={alt}
-    />
-    {underline && <div className="h-0.5 w-full bg-white/25 md:hidden" />}
-  </div>
-);
+const ImageBox = ({ src, alt, cover, underline, typeName = 'nav' }) => {
+  const types = {
+    destination:
+      'w-[10.63rem] md:w-[18.75rem] bpCustom:w-[27.81rem] aspect-square',
+    crew: 'w-[20.44rem] h-[14rem] md:w-[28.5rem] md:h-[35.75rem] bpCustom:w-[35.5rem] bpCustom:h-[44.5rem]',
+    technology:
+      'w-full h-[10.63rem] md:h-[19.38rem] bpCustom:w-[32.19rem] bpCustom:h-[32.94rem]',
+    nav: 'w-10 h-10 absolute top-6 left-6 md:top-7 md:left-10 bpCustom:left-14',
+  };
+
+  return (
+    <div className={types[typeName]}>
+      <img
+        className={`${cover ? 'object-cover' : 'object-contain'} h-full w-full`}
+        src={src}
+        alt={alt}
+      />
+      {underline && <div className="h-0.5 w-full bg-white/25 md:hidden" />}
+    </div>
+  );
+};
 
 export default ImageBox;
