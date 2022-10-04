8f9e5b4be7ad5431c13275379ad35fa8eef9fe90
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-04 11:56:42 -0600

    refactor: move ul component to Links - desktop version

---
+++ src/components/Navigation/Links.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { linkList } from '../../config';
+
+const Links = () => {
+  return (
+    <ul className="hidden h-full w-auto items-center gap-9 bg-white/20 px-12 md:flex lg:px-32">
+      {linkList.map((link) => (
+        <li key={link.id} className="h-full text-lg">
+          <Link
+            data-link
+            to={link.href}
+            className={`link-hover relative flex h-full items-center font-barlow uppercase tracking-wider text-white content-none ${
+              link.name === 'home' ? 'link-active' : ''
+            }`}
+          >
+            <span className="mr-2 hidden bpCustom:inline-block">
+              {link.span}
+            </span>
+            {link.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Links;
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { ImageBox } from '..';
-import MobileNav from './MobileNav';
 import HorLine from './HorLine';
+import Links from './Links';
+import MobileNav from './MobileNav';
 import logo from '/assets/shared/logo.svg';
-import { linkList } from '../../config';
 
 const Nav = () => {
   useEffect(() => {
@@ -21,24 +20,7 @@ const Nav = () => {
     <nav className="flex-end fixed top-0 left-1/2 z-50 flex h-auto w-full max-w-screen-bpCustom -translate-x-1/2 items-center justify-end md:h-24 bpCustom:top-10">
       <ImageBox src={logo} alt="logo" />
       <HorLine />
-      <ul className="hidden h-full w-auto items-center gap-9 bg-white/20 px-12 md:flex lg:px-32">
-        {linkList.map((link) => (
-          <li key={link.id} className="h-full text-lg">
-            <Link
-              data-link
-              to={link.href}
-              className={`link-hover relative flex h-full items-center font-barlow uppercase tracking-wider text-white content-none ${
-                link.name === 'home' ? 'link-active' : ''
-              }`}
-            >
-              <span className="mr-2 hidden bpCustom:inline-block">
-                {link.span}
-              </span>
-              {link.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Links />
       <MobileNav />
     </nav>
   );
