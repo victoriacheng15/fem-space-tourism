8a7bc2625eb001c9dd93df860cb24db2af4ff9c2
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-04 11:08:11 -0600

    refactor: rename to PageButton and change pagName to compName

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as InfoTitle } from './Titles/InfoTitle';
 export { default as PageTitle } from './Titles/PageTitle';
 export { default as Paragraph } from './Titles/Paragraph';
-export { default as Button } from './Button';
+export { default as PageButton } from './Button';
 export { default as ImageBox } from './ImageBox';
 export { default as Nav } from './Navigation';
 export { default as TravelInfo } from './TravelInfo';
