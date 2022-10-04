233ce79dc7e77de5838da10ebc04436012c873e7
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-04 11:11:35 -0600

    refactor: add ContentContainer Component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,4 @@
 export { default as MainContainer } from './MainContainer';
 export { default as HomeContainer } from './HomeContainer';
 export { default as PageContainer } from './PageContainer';
+export { default as ContentContainer } from './ContentContainer';
